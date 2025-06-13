6a51f04ae9b762ee18e99f7d331d03e82261c486
Mayank Saini <mayanksaini9389@gmail.com>
2025-06-13 11:24:35 +0530

    all

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -69,7 +69,7 @@ const Login = () => {
       toast.error("Failed to initiate Google login");
     }
   };
-
+// add
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-yellow-50 to-yellow-100 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8 bg-white p-8 rounded-xl shadow-lg">
