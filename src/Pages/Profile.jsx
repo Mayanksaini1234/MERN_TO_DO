09c844174ea838f571ef08e2c9b60f97cb796ec2
Mayank Saini <mayanksaini9389@gmail.com>
2025-06-28 12:12:52 +0530

    all

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -8,7 +8,10 @@ const Profile = () => {
   const { user, setUSer, loader, setLoader , isAuthenticated , autoloading } = useContext(ToDoContext);
 const navigate = useNavigate();
 
-if(!isAuthenticated) return navigate("/login")
+useEffect(()=>{
+
+  if(!isAuthenticated) return navigate("/login")
+})
   return (
     <div className="min-h-[calc(100vh-4rem)] flex items-center justify-center bg-gradient-to-b from-yellow-50 to-yellow-100 py-12 px-4 sm:px-6 lg:px-8">
       {loader ? (
